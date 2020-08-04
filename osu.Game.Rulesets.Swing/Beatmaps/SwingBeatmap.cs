3324b380496a490580a88fbcb76d42d90eb0047d
Andrei Zavatski <megaman9919@gmail.com>
2020-08-04 04:21:42 +0300

    Add beatmps statistics

--- osu.Game.Rulesets.Swing/Beatmaps/SwingBeatmap.cs
+++ osu.Game.Rulesets.Swing/Beatmaps/SwingBeatmap.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using osu.Framework.Graphics.Sprites;
 using osu.Game.Beatmaps;
 using osu.Game.Rulesets.Swing.Objects;
 
@@ -7,6 +8,26 @@ namespace osu.Game.Rulesets.Swing.Beatmaps
 {
     public class SwingBeatmap : Beatmap<SwingHitObject>
     {
-        public override IEnumerable<BeatmapStatistic> GetStatistics() => Array.Empty<BeatmapStatistic>();
+        public override IEnumerable<BeatmapStatistic> GetStatistics()
+        {
+            int taps = HitObjects.Count(s => s is Tap);
+            int spinners = HitObjects.Count(s => s is Spinner);
+
+            return new[]
+            {
+                new BeatmapStatistic
+                {
+                    Name = @"Tap Count",
+                    Content = taps.ToString(),
+                    Icon = FontAwesome.Regular.Circle
+                },
+                new BeatmapStatistic
+                {
+                    Name = @"Spinner Count",
+                    Content = spinners.ToString(),
+                    Icon = FontAwesome.Regular.Circle
+                }
+            };
+        }
     }
 }
