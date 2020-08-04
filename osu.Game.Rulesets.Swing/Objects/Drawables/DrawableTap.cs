8d24f9b94971e61983484c51a18afe37a96924ff
Andrei Zavatski <megaman9919@gmail.com>
2020-08-04 04:28:43 +0300

    Fix top and bottom taps couldn't be tapped at the same frame

--- osu.Game.Rulesets.Swing/Objects/Drawables/DrawableTap.cs
+++ osu.Game.Rulesets.Swing/Objects/Drawables/DrawableTap.cs
@@ -16,7 +16,6 @@ namespace osu.Game.Rulesets.Swing.Objects.Drawables
     public class DrawableTap : DrawableSwingHitObject<Tap>
     {
         private bool validActionPressed;
-        private bool pressHandledThisFrame;
         private readonly double rotationTime;
         private readonly double appearTime;
 
@@ -100,10 +99,6 @@ namespace osu.Game.Rulesets.Swing.Objects.Drawables
         {
             base.Update();
 
-            // The input manager processes all input prior to us updating, so this is the perfect time
-            // for us to remove the extra press blocking, before input is handled in the next frame
-            pressHandledThisFrame = false;
-
             if (Result.HasResult && Result.Type != HitResult.Miss)
                 return;
 
@@ -152,8 +147,6 @@ namespace osu.Game.Rulesets.Swing.Objects.Drawables
 
         public override bool OnPressed(SwingAction action)
         {
-            if (pressHandledThisFrame)
-                return true;
             if (Judged)
                 return false;
 
@@ -164,9 +157,6 @@ namespace osu.Game.Rulesets.Swing.Objects.Drawables
             if (IsHit)
                 HitAction = action;
 
-            // Regardless of whether we've hit or not, any secondary key presses in the same frame should be discarded
-            // E.g. hitting a non-strong centre as a strong should not fall through and perform a hit on the next note
-            pressHandledThisFrame = true;
             return result;
         }
 
