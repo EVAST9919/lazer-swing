f283b5947451b7c0968879a28f8636cd76d91c33
Andrei Zavatski <megaman9919@gmail.com>
2020-08-04 04:33:44 +0300

    Add gradient to playfield line

--- osu.Game.Rulesets.Swing/UI/SwingPlayfield.cs
+++ osu.Game.Rulesets.Swing/UI/SwingPlayfield.cs
@@ -7,6 +7,9 @@ using osu.Framework.Graphics.Shapes;
 using osu.Game.Rulesets.Swing.Objects;
 using osu.Game.Rulesets.Swing.Configuration;
 using osu.Framework.Bindables;
+using osu.Framework.Graphics.Colour;
+using osuTK.Graphics;
+using osu.Framework.Extensions.Color4Extensions;
 
 namespace osu.Game.Rulesets.Swing.UI
 {
@@ -38,7 +41,8 @@ namespace osu.Game.Rulesets.Swing.UI
                         Origin = Anchor.TopCentre,
                         Height = FULL_SIZE.Y / 2 - SwingHitObject.DEFAULT_SIZE / 2,
                         Width = 1,
-                        EdgeSmoothness = Vector2.One
+                        EdgeSmoothness = Vector2.One,
+                        Colour = ColourInfo.GradientVertical(Color4.Black.Opacity(0), Color4.White)
                     },
                     new Box
                     {
@@ -46,7 +50,8 @@ namespace osu.Game.Rulesets.Swing.UI
                         Origin = Anchor.BottomCentre,
                         Height = FULL_SIZE.Y / 2 - SwingHitObject.DEFAULT_SIZE / 2,
                         Width = 1,
-                        EdgeSmoothness = Vector2.One
+                        EdgeSmoothness = Vector2.One,
+                        Colour = ColourInfo.GradientVertical(Color4.White, Color4.Black.Opacity(0))
                     },
                     new Ring
                     {
