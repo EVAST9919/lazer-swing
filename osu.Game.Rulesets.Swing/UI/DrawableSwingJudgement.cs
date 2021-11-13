d7ea25a862190c974560d2978a80b9414d97a8d6
Andrei Zavatski <megaman9919@gmail.com>
2021-11-13 17:35:39 +0300

    Improve judgement animation

--- osu.Game.Rulesets.Swing/UI/DrawableSwingJudgement.cs
+++ osu.Game.Rulesets.Swing/UI/DrawableSwingJudgement.cs
@@ -30,5 +30,42 @@ namespace osu.Game.Rulesets.Swing.UI
                     break;
             }
         }
+
+        protected override Drawable CreateDefaultJudgement(HitResult result) => new DefaultSwingJudgementPiece(result);
+
+        private class DefaultSwingJudgementPiece : DefaultJudgementPiece
+        {
+            public DefaultSwingJudgementPiece(HitResult result)
+                : base(result)
+            {
+            }
+
+            protected override void LoadComplete()
+            {
+                base.LoadComplete();
+
+                JudgementText.Font = JudgementText.Font.With(size: 25);
+            }
+
+            public override void PlayAnimation()
+            {
+                switch (Result)
+                {
+                    case HitResult.None:
+                    case HitResult.Miss:
+                        base.PlayAnimation();
+                        break;
+
+                    default:
+                        this.ScaleTo(0.8f);
+                        this.ScaleTo(1, 250, Easing.OutElastic);
+
+                        this.Delay(50)
+                            .ScaleTo(0.75f, 250)
+                            .FadeOut(200);
+                        break;
+                }
+            }
+        }
     }
 }
