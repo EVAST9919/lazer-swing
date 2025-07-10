5293de7678b9c0d1ed1dda3cc79cd47e1515b3f1
Andrei Zavatski <megaman9919@gmail.com>
2025-07-11 02:06:01 +0300

    Fix ruleset crash

--- osu.Game.Rulesets.Swing/Scoring/SwingHitWindows.cs
+++ osu.Game.Rulesets.Swing/Scoring/SwingHitWindows.cs
@@ -1,15 +1,14 @@
-﻿using osu.Game.Rulesets.Scoring;
+﻿using System;
+using osu.Game.Beatmaps;
+using osu.Game.Rulesets.Scoring;
 
 namespace osu.Game.Rulesets.Swing.Scoring
 {
     public class SwingHitWindows : HitWindows
     {
-        private static readonly DifficultyRange[] swing_ranges =
-        {
-            new DifficultyRange(HitResult.Great, 50, 35, 20),
-            new DifficultyRange(HitResult.Good, 120, 80, 50),
-            new DifficultyRange(HitResult.Miss, 135, 95, 70),
-        };
+        private static readonly DifficultyRange great_window = new(50, 35, 20);
+        private static readonly DifficultyRange good_window = new(120, 80, 50);
+        private static readonly DifficultyRange miss_window = new(135, 95, 70);
 
         public override bool IsHitResultAllowed(HitResult result)
         {
@@ -24,6 +23,33 @@ namespace osu.Game.Rulesets.Swing.Scoring
             return false;
         }
 
-        protected override DifficultyRange[] GetRanges() => swing_ranges;
+        private double great;
+        private double good;
+        private double miss;
+
+        public override void SetDifficulty(double difficulty)
+        {
+            great = Math.Floor(IBeatmapDifficultyInfo.DifficultyRange(difficulty, great_window)) - 0.5;
+            good = Math.Floor(IBeatmapDifficultyInfo.DifficultyRange(difficulty, good_window)) - 0.5;
+            miss = Math.Floor(IBeatmapDifficultyInfo.DifficultyRange(difficulty, miss_window)) - 0.5;
+        }
+
+        public override double WindowFor(HitResult result)
+        {
+            switch (result)
+            {
+                case HitResult.Great:
+                    return great;
+
+                case HitResult.Good:
+                    return good;
+
+                case HitResult.Miss:
+                    return miss;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(result), result, null);
+            }
+        }
     }
 }
